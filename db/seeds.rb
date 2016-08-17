3be43c745349e531764d84c8af5c979de144791d
Max Philips <maximilien.philips@gmail.com>
2016-08-17 17:31:29 +0100

    new exercice

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name:"Dishoom", address:"shoreditch", phone_number:"+4455994433", category:"chinese")
+Restaurant.create(name:"Belgo", address:"Moortgate", phone_number:"+4455994434", category:"belgian")
+Restaurant.create(name:"Napulo", address:"Fulham", phone_number:"+4455994435", category:"italian")
+Restaurant.create(name:"Flat iron", address:"Covent Garden", phone_number:"+4455994436", category:"french")
+Restaurant.create(name:"Kamo", address:"Uccle", phone_number:"+4455994437", category:"japanese")
